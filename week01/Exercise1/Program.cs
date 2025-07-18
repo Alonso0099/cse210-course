0be381813d61dbbcfdb3908797199ef70e276972
Alonso Alvarado <alonsoj.alvarado@gmail.com>
2025-07-18 17:39:42 -0600

    testing

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -8,7 +8,7 @@ class Program
 
         Console.Write("Whats is your first name? ");
         string fname = Console.ReadLine();
-        Console.Write("What is your last name? ");
+        Console.Write("What is your last names? ");
         string lname = Console.ReadLine();
         Console.WriteLine($"Your name is {lname}, {fname} {lname} ");
 
