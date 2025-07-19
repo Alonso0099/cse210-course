25fa18cea123e605e8febb1b1b7202239dddf06a
Alonso Alvarado <alonsoj.alvarado@gmail.com>
2025-07-18 19:10:41 -0600

    Excercise completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "The Aroma Trace";
+        job1._jobTitle = "Technician";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Alonso Alvarado";
+        resume1._jobs.Add(job1);
+
+        resume1.Display();
     }
 }
