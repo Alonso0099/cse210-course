0a0bea2ddef024c31c78ddcefddfacf394eb3128
Alonso Alvarado <alonsoj.alvarado@gmail.com>
2025-08-03 00:25:19 -0600

    Homework Assignment completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment myAssignment = new Assignment("Alonso Alvarado", "Multiplication");
+        MathAssignment myAssignment1 = new MathAssignment("Alonso Alvarado", "Fractions", "7.3", "8-9");
+        WritingAssignment myWritingAssignment = new WritingAssignment("Alonso", "European History", "The Causes of World War II");
+        Console.WriteLine(myAssignment.GetSummary());
+        Console.WriteLine($"{myAssignment1.GetHomeworkList()}\n");
+        
+        Console.WriteLine(myWritingAssignment.GetSummary());
+        Console.WriteLine(myWritingAssignment.GetWritingAssignment());
+
+        
+        
+
     }
 }
