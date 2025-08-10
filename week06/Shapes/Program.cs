a7f36a24b6d6999240a71e0dfb4bc1c52ff0598a
Alonso Alvarado <alonsoj.alvarado@gmail.com>
2025-08-09 19:48:14 -0600

    Shapes program completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,26 @@
 using System;
-
+using System.Security.Cryptography.X509Certificates;
+//GetType added
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shapes> shapes = new List<Shapes>();
+        Square square1 = new Square("Blue", 5);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("Green", 4, 6);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle(8, "Purple");
+        shapes.Add(circle1);
+
+        foreach (Shapes shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {shape.GetType().Name} of color {color} has an area of {area:F2}");
+        }
     }
 }
---
+++ week06/Shapes/Shapes.cs
@@ -0,0 +1,26 @@
+using System.Drawing;
+using System.Reflection.Metadata;
+
+public abstract class Shapes
+{
+
+    private string _color;
+
+    public Shapes(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+}
