108a7217953cd9f72a247e75d0c933f5f3c25899
Alonso Alvarado <alonsoj.alvarado@gmail.com>
2025-08-14 11:42:41 -0600

    Assignment completed

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("03 Ago 2025",30,4.8),
+            new Cycling("03 Ago 2025",45,15.0),
+            new Swimming("03 Ago 2025",25,30)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
+
+ 
 }
